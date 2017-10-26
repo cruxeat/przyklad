75d5c2851b789c07b56f74c755cbf1ae1fbda16a
cruxeat <32929882+cruxeat@users.noreply.github.com>
2017-10-26 17:44:14 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField textfield;
+    public Label label;
+
+    public void handleCLICK(ActionEvent actionEvent) {
+        textfield.setText("O, umiesz klikac");
+        // dupa blada
+        // Co tam Janek slychac?
+        // okej
+        
+    }
+}
